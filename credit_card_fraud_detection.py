bf7695f18e51402c38b98aac99f2e459ecf319ca
Yusuf Babatunde <97997214+BIGTUNZ@users.noreply.github.com>
2024-11-20 17:52:45 +0100

    Update credit_card_fraud_detection.py

--- credit_card_fraud_detection.py
+++ credit_card_fraud_detection.py
@@ -18,7 +18,7 @@ st.header('Input Features')
 # Feature sliders with appropriate ranges based on your dataset
 v11 = st.slider('V11', min_value=-20.0, max_value=20.0, value=0.0, step=0.1)
 v4 = st.slider('V4', min_value=-20.0, max_value=20.0, value=0.0, step=0.1)
-v2 = st.slider('V2', min_value=-20.0, max_value=20.0, value=0.0, step=0.1)
+v3 = st.slider('V3', min_value=-20.0, max_value=20.0, value=0.0, step=0.1)
 v17 = st.slider('V17', min_value=-20.0, max_value=20.0, value=0.0, step=0.1)
 v14 = st.slider('V14', min_value=-20.0, max_value=20.0, value=0.0, step=0.1)
 v12 = st.slider('V12', min_value=-20.0, max_value=20.0, value=0.0, step=0.1)
